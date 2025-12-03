6b671e563315db52510234b0e05090c3c0f74d34
Yvan04444 <163105082+Yvan04444@users.noreply.github.com>
2025-12-03 10:06:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>
+    return "<h2>Ma page de contact</h2>"
   
 if __name__ == "__main__":
   app.run(debug=True)
